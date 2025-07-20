d3aaea4d2ca316108318f47e079690f08549e725
にっしー <Nishiii4405@gmail.com>
2025-07-20 05:17:01 +0000

    コメント削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-"use client"; // これを追加
+"use client";
 
-import Head from 'next/head';
-import { useState, useEffect } from 'react';
-import Header from './base/Header';
-import { groups } from './const/group/GroupList';
-import Footer from './base/Footer';
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import Header from "./base/Header";
+import { groups } from "./const/group/GroupList";
+import Footer from "./base/Footer";
 
 export default function Home() {
   const [scrollOpacity, setScrollOpacity] = useState(1);
@@ -20,8 +20,8 @@ export default function Home() {
       }
     }
 
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
@@ -31,7 +31,9 @@ export default function Home() {
       </Head>
       <Header bgColor="bg-black" textColor="text-white" />
       <div className="bg-black text-white flex flex-col items-center justify-center py-20 min-h-screen tracking-lwidest">
-        <h1 className="text-3xl md:text-7xl text-center font-bold animate-fadeInUp delay-100">同志社大学VOCALOID研究会Arpeggio</h1>
+        <h1 className="text-3xl md:text-7xl text-center font-bold animate-fadeInUp delay-100">
+          同志社大学VOCALOID研究会Arpeggio
+        </h1>
         <p
           className="absolute bottom-10 text-lg animate-bouncePulse transition-opacity duration-500"
           style={{ opacity: scrollOpacity }}
@@ -51,12 +53,18 @@ export default function Home() {
         </p>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mt-5">
           {groups.map((group) => (
-            <div key={group.id} className="bg-transparent border border-mikuBlue text-white rounded-lg p-6 text-center transition-transform">
+            <div
+              key={group.id}
+              className="bg-transparent border border-mikuBlue text-white rounded-lg p-6 text-center transition-transform"
+            >
               <h3 className="text-2xl font-semibold">{group.name}</h3>
             </div>
-          ))} 
+          ))}
         </div>
-        <a href="/activity" className="mt-10 bg-mikuBlue text-white font-semibold text-2xl px-10 py-4 rounded-lg transition-colors hover:scale-105">
+        <a
+          href="/activity"
+          className="mt-10 bg-mikuBlue text-white font-semibold text-2xl px-10 py-4 rounded-lg transition-colors hover:scale-105"
+        >
           詳細な活動内容を見る
         </a>
       </div>
@@ -67,16 +75,20 @@ export default function Home() {
           Arpeggioはとにかく面白い人や歌が上手な人、神絵師さんなど、個性豊かな人たちがたくさんいます
         </p>
         <div className="flex space-x-4 mt-10">
-          <a href="https://note.com/arpeggiovocaloid/"
+          <a
+            href="https://note.com/arpeggiovocaloid/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-mikuBlue text-2xl px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors">
+            className="text-mikuBlue text-2xl px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors"
+          >
             noteブログ
           </a>
-          <a href="http://arpeggiod.blog90.fc2.com/"
+          <a
+            href="http://arpeggiod.blog90.fc2.com/"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-mikuBlue text-2xl  px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors">
+            className="text-mikuBlue text-2xl  px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors"
+          >
             fc2ブログ
           </a>
         </div>
@@ -85,13 +97,16 @@ export default function Home() {
       <div className="bg-black text-white flex flex-col items-center justify-center py-10 tracking-wider mx-10">
         <h2 className="text-4xl font-bold">Arpeggioに興味がある！</h2>
         <p className="text-xl mt-5 mx-10">
-          Arpeggioの活動詳細やコンタクトはX (Twitter)から受け付けています。ぜひDMにてご連絡ください！
+          Arpeggioの活動詳細やコンタクトはX
+          (Twitter)から受け付けています。ぜひDMにてご連絡ください！
         </p>
         <div className="flex space-x-4 mt-10">
-          <a href="https://twitter.com/arpeggio_kouhou"
+          <a
+            href="https://twitter.com/arpeggio_kouhou"
             target="_blank"
             rel="noopener noreferrer"
-            className="text-mikuBlue text-2xl  px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors">
+            className="text-mikuBlue text-2xl  px-6 py-3 rounded-lg border-2 border-transparent hover:border-mikuPink transition-colors"
+          >
             X (Twitter)を見る
           </a>
         </div>
